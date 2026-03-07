2ab665c62c221f24b4563fcb568566bdfefd7c95
gabriel48fernandes <gabrielfernandesacdc@gmail.com>
2026-03-07 11:23:10 -0300

    .

--- frontend/loja/script.js
+++ frontend/loja/script.js
@@ -26,7 +26,7 @@ if (areaUsuario) {
     areaUsuario.innerHTML = `
       <span>Olá, ${primeiroNome} 👋</span>
 
-      <a href="/loja/carrinho.html" class="icon-link">🛒</a>
+      <a href="/carrinho.html" class="icon-link">🛒</a>
 
       ${auth.usuario.role === "admin"
         ? `<a href="/admin/admin.html" class="btn-admin">⚙ ADM</a>`
