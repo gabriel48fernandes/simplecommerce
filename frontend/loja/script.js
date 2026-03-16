543a8b983ce82743315ce4e2b3da5fa690db22f0
gabriel48fernandes <gabrielfernandesacdc@gmail.com>
2026-03-15 23:37:19 -0300

    .

--- frontend/loja/script.js
+++ frontend/loja/script.js
@@ -56,6 +56,35 @@ if (areaUsuario) {
 
 }
 
+let currentSlide = 0
+
+const slides = document.querySelectorAll(".slide")
+const dots = document.querySelectorAll(".dot")
+
+function showSlide(index){
+
+ slides.forEach(slide => slide.classList.remove("active"))
+ dots.forEach(dot => dot.classList.remove("active"))
+
+ slides[index].classList.add("active")
+ dots[index].classList.add("active")
+
+}
+
+function nextSlide(){
+
+ currentSlide++
+
+ if(currentSlide >= slides.length){
+  currentSlide = 0
+ }
+
+ showSlide(currentSlide)
+
+}
+
+setInterval(nextSlide,4000)
+
 function animarProdutoCarrinho(imagemProduto) {
 
   const carrinho = document.getElementById("iconeCarrinho");
